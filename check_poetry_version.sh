7aa5164603176534b66bd73dbd38fe2f295ff902
Kiwinicki <szefek24@gmail.com>
2024-11-17 21:28:16 +0100

    fetch before poetry ver check

--- check_poetry_version.sh
+++ check_poetry_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git fetch origin main
+
 base_version=$(git show origin/main:pyproject.toml | grep '^version = ' | cut -d '"' -f2)
 branch_version=$(grep '^version = ' pyproject.toml | cut -d '"' -f2)
 
