6a5fee4cdd6518fb66d120f157b2dad657b6db9b
Jacek Nowak <jaceknowakdev@gmail.com>
2024-11-19 13:20:47 +0100

    initialize spring app

---
+++ webapp/src/main/java/dev/jlynx/magical_drones_webapp/MagicalDronesApp.java
@@ -0,0 +1,13 @@
+package dev.jlynx.magical_drones_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicalDronesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicalDronesApp.class, args);
+    }
+
+}
