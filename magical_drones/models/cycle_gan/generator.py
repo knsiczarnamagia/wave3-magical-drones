5f969b3594f49966284ea228fd4e402476c75917
Jan Karas <mygnirut@gmail.com>
2024-12-21 19:31:25 +0100

    Implementing CycleGAN Generator baseline

--- magical_drones/models/cycle_gan/generator.py
+++ magical_drones/models/cycle_gan/generator.py
@@ -7,15 +7,83 @@ from magical_drones.models import BaseGenerator
 
 class Generator(BaseGenerator):
     def __init__(
-        self, input_channels: int = 3, num_features: int = 64, num_residuals: int = 2
+            self, input_channels: int = 3, num_features: int = 64, num_residuals: int = 2
     ):
         super().__init__()
-        # self.initial =
-        # self.down_blocks =
-        # self.residual_blocks =
-        # self.up_blocks =
-        # self.last_block  =
-        pass
+        self.model = self._construct_model()
+
+    def _construct_model(self):
+        initial_layer = nn.Sequential(
+            nn.Conv2d(
+                self.input_channels,
+                self.num_features,
+                kernel_size=7,
+                stride=1,
+                padding=3,
+                padding_mode="reflect",
+            ),
+            nn.ReLU(inplace=True),
+        )
+
+        down_blocks = nn.ModuleList(
+            [
+                ConvBlock(
+                    self.num_features, self.num_features * 2, kernel_size=3, stride=2, padding=1
+                ),
+                ConvBlock(
+                    self.num_features * 2,
+                    self.num_features * 4,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                ),
+            ]
+        )
+
+        residual_blocks = nn.Sequential(
+            *[ResidualBlock(self.num_features * 4) for _ in range(self.num_features)]
+        )
+
+        up_blocks = nn.ModuleList(
+            [
+                ConvBlock(
+                    self.num_features * 4,
+                    self.num_features * 2,
+                    down=False,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+                ConvBlock(
+                    self.num_features * 2,
+                    self.num_features,
+                    down=False,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+            ]
+        )
+
+        last_block = nn.Conv2d(
+            self.num_features,
+            self.input_channels,
+            kernel_size=7,
+            stride=1,
+            padding=3,
+            padding_mode="reflect",
+        )
+
+        model = nn.Sequential(
+            initial_layer,
+            down_blocks,
+            residual_blocks,
+            up_blocks,
+            last_block
+        )
+        return model
 
     def forward(self, noise: Tensor) -> Tensor:
         x = self.initial(noise)
@@ -31,11 +99,9 @@ class Generator(BaseGenerator):
         return tanh(self.last_block(x))
 
 
-
-
 class ConvBlock(nn.Module):
     def __init__(
-        self, in_channels, out_channels, down=True, use_activation=True, **kwargs
+            self, in_channels, out_channels, down=True, use_activation=True, **kwargs
     ):
         super().__init__()
         self.conv = nn.Sequential(
@@ -46,12 +112,12 @@ class ConvBlock(nn.Module):
             nn.ReLU(inplace=True) if use_activation else nn.Identity(),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return self.conv(x)
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, channels):
+    def __init__(self, channels: int):
         super().__init__()
         self.block = nn.Sequential(
             ConvBlock(channels, channels, kernel_size=3, padding=1),
@@ -60,5 +126,5 @@ class ResidualBlock(nn.Module):
             ),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return x + self.block(x)
