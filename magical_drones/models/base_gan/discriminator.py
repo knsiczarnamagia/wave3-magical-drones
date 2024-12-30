c8757e31fd4840a89497bef4d6b54c3a565c83fd
Remi <remigiusz.sek@gmail.com>
2024-12-30 15:39:12 +0100

    Update the discriminator code

--- magical_drones/models/base_gan/discriminator.py
+++ magical_drones/models/base_gan/discriminator.py
@@ -1,13 +1,19 @@
 from abc import ABC, abstractmethod
 
-from torch import nn
+from torch import nn, Tensor
 
 
 class BaseDiscriminator(ABC, nn.Module):
-    def __init__(self):
+    def __init__(
+            self,
+            input_channels: int = 3,
+            features: list = None,
+    ):
         super().__init__()
-        pass
+        self.input_channels = input_channels
+        if features is None:
+            self.features = [64, 128, 256, 512]
 
     @abstractmethod
-    def forward(self):
+    def forward(self, z: Tensor) -> Tensor:
         pass
