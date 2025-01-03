013772ee5072f20ea0f880538a22523ce5c98089
Remi <remigiusz.sek@gmail.com>
2025-01-03 16:22:28 +0100

    Finish trainer after main positive test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from magical_drones.models.cycle_gan.gan import CycleGAN
+import torch
 
+print(torch.randn(1, 3, 224, 224))
 
-model = CycleGAN()
-# TODO: Implement lightning trainer
+model = CycleGAN(channels=3, width=224, height=224)
+
+prediction = model(torch.randn(1, 3, 224, 224))
