2513660c2f7b4731b639a828ccb389e3495e6642
Andrii Norets <104683293+AndriiNorets@users.noreply.github.com>
2025-01-03 15:13:26 +0100

    DATASET | MagMapV1 DataModule Baseline (#13)
    * Remove legacy CycleGAN
    
    * First steps of Lightning structure
    
    * CycleGAN
    
    * Implementing CycleGAN Generator baseline
    
    * Fix format
    
    * Implement DataModule
    
    * Move test to the tests folder
    
    * Update packages
    
    ---------
    
    Co-authored-by: Jan Karas <mygnirut@gmail.com>
    Co-authored-by: Andrii Norets <104683293+KabinetFnsv@users.noreply.github.com>

--- magical_drones/datasets/magmap.py
+++ magical_drones/datasets/magmap.py
@@ -1,5 +1,69 @@
 from pytorch_lightning import LightningDataModule
+from torch.utils.data import Dataset, DataLoader
+from datasets import load_dataset
+from PIL import Image
 
 
-class MagMapV1(LightningDataModule):  # TODO: WIll be implemented in other task
-    pass
+class MagMapDataSet(Dataset):
+
+    def __init__(self, data , transform):
+        self.data = data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        try:
+            sat_image = Image.open(sample["sat_image"]) if isinstance(sample["sat_image"], str) else sample["sat_image"]
+            map_image = Image.open(sample["map_image"]) if isinstance(sample["map_image"], str) else sample["map_image"]
+
+            sat_image = self.transform(sat_image)
+            map_image = self.transform(map_image)
+            
+        except Exception as e:
+            raise ValueError(f"Error loading image at index {idx}: {e}")
+        
+        return {"sat_image": sat_image, "map_image": map_image}
+
+
+
+
+class MagMapV1(LightningDataModule): 
+
+    def __init__(self, data_link, batch_size, transform):
+        super().__init__()
+        self.data_link = data_link
+        self.batch_size = batch_size
+        self.transform = transform
+
+    def setup(self, stage: str=None):
+        # data_dict = load_dataset(self.data_link, split="train", data_files="train-00000-of-00015.parquet")
+        data_dict = load_dataset(
+            self.data_link,
+        )
+        data = data_dict["train"] 
+
+        total_len = len(data)
+        train_len = int(0.8 * total_len)
+        val_len = int(0.1 * total_len)
+
+        self.train_dataset = MagMapDataSet(data.select(range(0, train_len)), 
+                                           transform = self.transform)
+        self.val_dataset = MagMapDataSet(data.select(range(train_len, train_len + val_len)), 
+                                         transform = self.transform)
+        self.test_dataset = MagMapDataSet(data.select(range(train_len + val_len, total_len)), 
+                                          transform = self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+
--- magical_drones/models/cycle_gan/gan.py
+++ magical_drones/models/cycle_gan/gan.py
@@ -16,19 +16,10 @@ class CycleGAN(BaseGAN):
         lr: float = 0.0002,
         b1: float = 0.5,
         b2: float = 0.999,
+        batch_size: int = 32,
         **kwargs,
     ):
-        super().__init__(
-            channels=channels,
-            width=width,
-            height=height,
-            latent_dim=latent_dim,
-            lr=lr,
-            b1=b1,
-            b2=b2,
-            **kwargs,
-        )
-        self.generated_imgs = None
+        super().__init__()
         self.save_hyperparameters()
         self.automatic_optimization = False
         self.generator = Generator()
@@ -98,3 +89,66 @@ class CycleGAN(BaseGAN):
 
     def on_validation_epoch_end(self):
         pass
+
+        optimizer_g, optimizer_d = self.optimizers()
+
+        z = torch.randn(images.shape[0], self.hparams.latent_dim)
+        z = z.type_as(images)
+
+        self.toggle_optimizer(optimizer_g)
+        self.generated_imgs = self(z)
+
+        # sample_images = self.generated_imgs[:6]
+        # grid = torchvision.utils.make_grid(sample_images)
+        # self.logger.experiment.add_image("train/generated_images", grid, self.current_epoch)
+
+        valid = torch.ones(images.size(0), 1)
+        valid = valid.type_as(images)
+
+        # TODO: Wrapp loss calculation in helper function?
+        g_loss = self.adversarial_loss(self.discriminator(self.generated_imgs), valid)
+        self.log("g_loss", g_loss, prog_bar=True)
+        self.manual_backward(g_loss)
+        optimizer_g.step()
+        optimizer_g.zero_grad()
+        self.untoggle_optimizer(optimizer_g)
+
+        self.toggle_optimizer(optimizer_d)
+
+        valid = torch.ones(images.size(0), 1)
+        valid = valid.type_as(images)
+
+        real_loss = self.adversarial_loss(self.discriminator(images), valid)
+
+        fake = torch.zeros(images.size(0), 1)
+        fake = fake.type_as(images)
+
+        fake_loss = self.adversarial_loss(
+            self.discriminator(self.generated_imgs.detach()), fake
+        )
+
+        d_loss = (real_loss + fake_loss) / 2
+        self.log("d_loss", d_loss, prog_bar=True)
+        self.manual_backward(d_loss)
+        optimizer_d.step()
+        optimizer_d.zero_grad()
+        self.untoggle_optimizer(optimizer_d)
+
+    def validation_step(self, batch: Tensor, batch_idx: int) -> None:
+        pass
+
+    def configure_optimizers(self):
+        lr = self.hparams.lr
+        b1 = self.hparams.b1
+        b2 = self.hparams.b2
+
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr, betas=(b1, b2))
+        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=lr, betas=(b1, b2))
+        return [opt_g, opt_d], []
+
+    def on_validation_epoch_end(self):
+        z = self.validation_z.type_as(self.generator.model[0].weight)
+
+        # sample_images = self(z)
+        # grid = torchvision.utils.make_grid(sample_imgs)
+        # self.logger.experiment.add_image("validation/generated_images", grid, self.current_epoch)
