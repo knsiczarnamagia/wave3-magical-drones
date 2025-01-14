436d54445d4ceb5a4553644ed152c2856f01b3b5
Dawid Koterwas <73834399+Kiwinicki@users.noreply.github.com>
2025-01-14 13:28:42 +0100

    Towards CycleGAN training (#24)
    Lightinig trainer, features:
    - logging to wandb
    - parametrized augmentations, model and training from yaml
    - mixed precision, benchmarking and other speedups

--- magical_drones/datasets/magmap.py
+++ magical_drones/datasets/magmap.py
@@ -1,13 +1,17 @@
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import Dataset, DataLoader
 from datasets import load_dataset
-from PIL import Image
+from torchvision.transforms import v2
+import torch
 
 
 class MagMapDataSet(Dataset):
-    def __init__(self, data, transform):
+    def __init__(self, data, transform=None):
         self.data = data
-        self.transform = transform
+        self.transform = transform if transform is not None else self._to_tensor()
+
+    def _to_tensor(self):  # equivalent to ToTensor which will be deprecated
+        return v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
 
     def __len__(self):
         return len(self.data)
@@ -15,38 +19,37 @@ class MagMapDataSet(Dataset):
     def __getitem__(self, idx):
         sample = self.data[idx]
         try:
-            sat_image = (
-                Image.open(sample["sat_image"])
-                if isinstance(sample["sat_image"], str)
-                else sample["sat_image"]
+            sat_image, map_image = self.transform(
+                sample["sat_image"].convert("RGB"),
+                sample["map_image"].convert("RGB"),
             )
-            map_image = (
-                Image.open(sample["map_image"])
-                if isinstance(sample["map_image"], str)
-                else sample["map_image"]
-            )
-
-            sat_image = self.transform(sat_image)
-            map_image = self.transform(map_image)
-
         except Exception as e:
-            raise ValueError(f"Error loading image at index {idx}: {e}")
+            raise ValueError(f"Error loading or transforming image at index {idx}: {e}")
 
-        return {"sat_image": sat_image, "map_image": map_image}
+        return (sat_image, map_image)
 
 
 class MagMapV1(LightningDataModule):
-    def __init__(self, data_link, batch_size, transform):
+    def __init__(
+        self,
+        data_link,
+        batch_size,
+        train_transform=None,
+        valid_transform=None,
+        test_transform=None,
+        num_workers=0,
+        **kwargs,
+    ):
         super().__init__()
         self.data_link = data_link
         self.batch_size = batch_size
-        self.transform = transform
+        self.train_transform = train_transform
+        self.valid_transform = valid_transform
+        self.test_transform = test_transform
+        self.num_workers = num_workers
 
     def setup(self, stage: str = None):
-        # data_dict = load_dataset(self.data_link, split="train", data_files="train-00000-of-00015.parquet")
-        data_dict = load_dataset(
-            self.data_link,
-        )
+        data_dict = load_dataset(self.data_link)
         data = data_dict["train"]
 
         total_len = len(data)
@@ -54,20 +57,61 @@ class MagMapV1(LightningDataModule):
         val_len = int(0.1 * total_len)
 
         self.train_dataset = MagMapDataSet(
-            data.select(range(0, train_len)), transform=self.transform
+            data.select(range(0, train_len)), transform=self.train_transform
         )
         self.val_dataset = MagMapDataSet(
-            data.select(range(train_len, train_len + val_len)), transform=self.transform
+            data.select(range(train_len, train_len + val_len)),
+            transform=self.valid_transform,
         )
         self.test_dataset = MagMapDataSet(
-            data.select(range(train_len + val_len, total_len)), transform=self.transform
+            data.select(range(train_len + val_len, total_len)),
+            transform=self.test_transform,
         )
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=True,
+            prefetch_factor=4,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size * 8,
+            num_workers=self.num_workers,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size * 8,
+            num_workers=self.num_workers,
+        )
+
+
+def make_tfms(
+    size: int = 256,
+    degrees: int = 0,
+    translate: tuple[float] | None = None,
+    flip_p: float = 0.0,
+    scale: tuple[float] | None = None,
+    shear: tuple[float] | None = None,
+):
+    return v2.Compose(
+        [
+            v2.ToImage(),
+            v2.Resize(size=size),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            v2.RandomHorizontalFlip(flip_p) if flip_p > 0 else v2.Identity(),
+            v2.RandomAffine(
+                degrees=degrees, translate=translate, scale=scale, shear=shear
+            )
+            if degrees or translate or scale or shear
+            else v2.Identity(),
+        ]
+    )
